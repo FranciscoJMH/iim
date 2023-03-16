7f1fe82ab5c3a628e168441fafb3bebb370e3c91
FJ_Michel <jav_michel@hotmail.com>
2023-03-16 17:20:19 -0600

    Articles view update

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :provider
 
-  has_many :buy_articles
+  has_many :buy_articles, dependent: :delete_all
   has_many :buys, through: :buy_articles
 end
 
